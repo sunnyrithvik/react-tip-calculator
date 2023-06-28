c47a86e593cd9760b0d6220e2ac36cb5253c1b39
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-06-28 20:32:30 +0530

    added extra components

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function Tip() {
   const [per1, setPer1] = useState(0);
   const [per2, setPer2] = useState(0);
 
+  const tip = ((per1 + per2) * bill) / 200;
+
   function handleChange1(e) {
     return setPer1(Number(e.target.value));
   }
@@ -29,6 +31,21 @@ function Tip() {
 
   return (
     <div>
+      <Bill bill={bill} setBill={setBill} />
+      <ServiceBill per={per1} onChange={handleChange1}>
+        select your service
+      </ServiceBill>
+      <ServiceBill per={per2} onChange={handleChange2}>
+        select friend service
+      </ServiceBill>
+      <Output bill={bill} tip={tip} onReset={handleReset} />
+    </div>
+  );
+}
+
+function Bill({ bill, setBill }) {
+  return (
+    <div>
       <p>How much was the bill?</p>
       <input
         type="text"
@@ -36,35 +53,36 @@ function Tip() {
         value={bill}
         onChange={(e) => setBill(Number(e.target.value))}
       />
-      <ServiceBill per={per1} onChange={handleChange1} />
-      <ServiceBill per={per2} onChange={handleChange2} />
-      {bill ? (
-        <div>
-          <h3>
-            {`you pay Rs. ${
-              bill + ((per1 + per2) * bill) / 200
-            } (Rs.${bill} + Rs.${((per1 + per2) * bill) / 200} tip)`}
-          </h3>
-
-          <button onClick={handleReset}>Reset</button>
-        </div>
-      ) : (
-        ""
-      )}
     </div>
   );
 }
 
-function ServiceBill({ per, onChange }) {
+function ServiceBill({ per, onChange, children }) {
   return (
     <div>
-      <p>how much service bill</p>
+      <p>{children}</p>
       <select value={per} onChange={(e) => onChange(e)}>
         <option value={0} key={0}>{`dissatisfied (0%)`}</option>
         <option value={5} key={5}>{`okay (5%)`}</option>
         <option value={10} key={10}>{`good (10%)`}</option>
         <option value={20} key={20}>{`amazing (20%)`}</option>
       </select>
+    </div>
+  );
+}
+
+function Output({ bill, tip, onReset }) {
+  return (
+    <div>
+      {bill ? (
+        <div>
+          <h3>{`you pay Rs. ${bill + tip} (Rs.${bill} + Rs.${tip} tip)`}</h3>
+
+          <button onClick={onReset}>Reset</button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
